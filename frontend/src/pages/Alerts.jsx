b619ba51d37da92f8f663ee7a76252851b79ad5a
SamashaHettiarachchi <sashinisamasha@gmail.com>
2024-10-13 20:30:34 +0530

    safty

--- frontend/src/pages/Alerts.jsx
+++ frontend/src/pages/Alerts.jsx
@@ -10,7 +10,7 @@ const Alerts = () => {
       <div className="flex flex-grow mt-16"> {/* Main content area */}
         <Leftbar /> {/* Sidebar on larger screens */}
         <div className="flex-grow p-6 bg-gray-200 sm:ml-64 flex flex-col"> {/* Main content area */}
-          <h1 className="text-3xl font-bold">Alert page</h1>
+          <h1 className="text-3xl font-bold"> safty Alert page</h1>
           <p>This is where your main content will go, separate from the dashboard part and navigation part.</p>
           
         </div>
